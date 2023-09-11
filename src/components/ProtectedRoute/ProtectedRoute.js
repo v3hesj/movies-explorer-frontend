149ee9fc4616ed0dca0fb42c05aa407abe0015e5
Kilin Anton <antikilin@yandex.ru>
2023-09-11 17:36:22 +0300

    rout

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react';
 import { Navigate } from "react-router-dom";
 import Preloader from '../Preloader/Preloader';
 
 const ProtectedRoute = ({ children, isLoading, isLoggedIn }) => {
+  // console.log(isLoggedIn);
   if (isLoading)
     return <Preloader />;
-  if (isLoggedIn === 0)
-    return <Navigate to='/' />;
-  return children;
+  return isLoggedIn ? children : <Navigate to='/' />
 };
 
 export default ProtectedRoute;
